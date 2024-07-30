e9329be25e20b562f54d3e303eb175b5bed69f7d
Innovative-J <juan.cia05678@gmail.com>
2024-07-29 22:07:03 -0500

    updated index.js files and shapes.js with proper functions

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
-import { input } from '@inquirer/prompts';
+// importing modules
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { Triangle, Circle, Square } = require("./lib/shapes");
 
-const questions = require('index.js');
-inquirer
-.prommpt([
-    { type: "input",
+// prompt user using inquirer for logo questions
+inquirer.prompt([
+    {
+        type: "input",
         name: "text",
         message: "TEXT: Enter up to (3) Characters:",
+        validate: input => input.length <= 3 || 'Text should be up to 3 characters!'
     },
     {
         type: "input",
-        name: "text-color",
+        name: "textColor",
         message: "TEXT COLOR: Enter a color keyword (OR a hexadecimal number):",
     },
     {
-        type: "input",
+        type: "list",
         name: "shape",
-        message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):",
+        message: "Choose which shape you would like",
+        choices: ["Circle", "Square", "Triangle"],
     },
     {
-        type: "list",
-        name: "pixel-image",
-        message: "Choose which Pixel Image you would like?",
-        choices: ["Circle", "Square", "Triangle"],
+        type: "input",
+        name: "shapeColor",
+        message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):",
     },
 ])
 .then((response) => {
-    const { } = response;
+    const { text, textColor, shape, shapeColor } = response;
+    
+    let shapeInstance;
+    switch (shape) {
+        case 'Circle':
+            shapeInstance = new Circle();
+            break;
+        case 'Square':
+            shapeInstance = new Square();
+            break;
+        case 'Triangle':
+            shapeInstance = new Triangle();
+            break;
+    }
+// generating svg data
+    const svgContent = `
+        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            ${shapeInstance.render(shapeColor)}
+            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+        </svg>
+    `;
 
-    fs.appendFile('SVG', ({title, description, installation, usage, credits, license, tests, github, email}), (err) => {
-        if (err) {
-            console.error('Error writing README file:', err);
+    writeToFile('logo.svg', svgContent);
+});
+// writing data to file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        if (error) {
+            console.error(error);
         } else {
             console.log('SVG successfully created!');
         }
     });
-});
-
+}
